cb8ffb9e44deec9124b60ac885fa087ac9a6fbd9
Ralph Oliveira <ralphive@protonmail.com>
2021-08-03 11:49:37 +0100

    add s4 payload formatting

--- app.js
+++ app.js
@@ -3,15 +3,41 @@ var parse = require('csv-parse');
 var async = require('async');
 
 //run this app with node app.js <filename.csv>
-var inputFile= process.argv[2];
+var inputFile = process.argv[2];
 
-var parser = parse({delimiter: ','}, function (err, data) {
-  async.eachSeries(data, function (line, callback) {
-    console.log(line)
-      callback();
-  })
+var parser = parse({
+    delimiter: ','
+}, function (err, data) {
+    async.eachSeries(data, function (line, callback) {
+        var payload = csvline2SO(line)
+        console.log(payload)
+        callback();
+    })
 });
 
 fs.createReadStream(inputFile).pipe(parser);
 
-
+function csvline2SO(line) {
+    return {
+        "SalesOrderType": "OR",
+        "SalesOrganization": "1710",
+        "DistributionChannel": "10",
+        "OrganizationDivision": "00",
+        "SoldToParty": "17100002",
+        "SalesOrderDate": line[2] + "T04:00:00",
+        "PurchaseOrderByCustomer": "Created via OData - Analytics data load",
+        "to_Partner": [{
+            "PartnerFunction": "SH",
+            "Customer": "17100002"
+        }],
+        "to_Item": [{
+                "Material": line[0],
+                "RequestedQuantity": "1"
+            },
+            {
+                "Material": line[1],
+                "RequestedQuantity": "1"
+            }
+        ]
+    }
+}
