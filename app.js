0c58bc3838616d2b925cf2e3b9063e0f838104f5
Ralph Oliveira <ralphive@protonmail.com>
2021-08-05 15:29:47 +0100

    sales order creation

--- app.js
+++ app.js
@@ -2,20 +2,22 @@ const fs = require('fs');
 const parse = require('csv-parse');
 const async = require('async');
 const axios = require('axios');
+const transport = axios.create({
+})
 
 
 //run this app with node app.js <filename.csv> <s4Tenant:port> <user> <password>
 const inputFile = process.argv[2];
 const s4URL = process.argv[3]
 console.log(s4URL)
-
+var SO_CREATED=0;
 
 //Retrieve XSRF Token (used in POST)
 let getToken = function () {
     return new Promise(function (resolve, reject) {  
         console.log("Fetching CSRF token")  
         //return x-csrf-token required to make POST requests
-        axios.request({
+        transport.request({
             url: "/sap/opu/odata/SAP/API_SALES_ORDER_SRV/",
             method: "HEAD",
             baseURL: s4URL,
@@ -27,10 +29,43 @@ let getToken = function () {
             console.log(res.status+" - "+res.statusText)
             if(res.headers['x-csrf-token']){
                 console.log("CSRF-Token retrieved")
-                resolve(res.headers['x-csrf-token'])
+                var token= {
+                    "x-csrf-token": res.headers['x-csrf-token'],
+                    "Accept": "application/json",
+                    "Content-Type":"application/json",
+                    "Cookie": res.headers['set-cookie'],
+                  }
+                
+                resolve(token)
+                
             }else{
                 console.error("Couldn't reetrieve CSRF Token")
                 reject(err)
+            }
+        }).catch((err) => {
+            console.error(err)
+            reject(res)
+        });
+    })
+}
+
+let createSO = function (token, data) {
+    return new Promise(function (resolve, reject) {  
+        console.log("Creating Sales Order")  
+        transport.request({
+            url: "/sap/opu/odata/sap/API_SALES_ORDER_SRV/A_SalesOrder",
+            method: "POST",
+            baseURL: s4URL,
+            headers: token,
+            data: data
+        }).then((res) => {
+            console.log(res.status+" - "+res.statusText)
+            if(res.status == 201){
+                console.log("Sales Order Created Successully!")
+                resolve()
+            }else{
+                console.error("Couldn't create sales order")
+                reject(res)
             }
         }).catch((err) => {
             console.error(err)
@@ -42,18 +77,20 @@ let getToken = function () {
 let parser = parse({
     delimiter: ','
 }, function (err, data) {
-    async.eachSeries(data, function (line, callback) {
-        var payload = csvline2SO(line)
-        // console.log(payload)
-        callback();
+    getToken().then(token => {
+         async.eachSeries(data, function (line, callback) {
+            var data = csvline2SO(line)
+            createSO(token,data).then(()=>{
+                SO_CREATED++;
+                console.log(SO_CREATED+ " SO Created")
+            })
+            callback();
+        })
     })
 });
 
-getToken().then(token => {
-    fs.createReadStream(inputFile).pipe(parser);
 
-})
-
+fs.createReadStream(inputFile).pipe(parser);
 
 function csvline2SO(line) {
     return {
@@ -63,7 +100,7 @@ function csvline2SO(line) {
         "OrganizationDivision": "00",
         "SoldToParty": "17100002",
         "SalesOrderDate": line[2] + "T04:00:00",
-        "PurchaseOrderByCustomer": "Created via OData - Analytics data load",
+        "PurchaseOrderByCustomer": "Analytics data load",
         "to_Partner": [{
             "PartnerFunction": "SH",
             "Customer": "17100002"
