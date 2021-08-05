cec072f1a78000990f0b5a4819da00105632dd26
Ralph Oliveira <ralphive@protonmail.com>
2021-08-05 14:23:41 +0100

    add function to retrieve csrf token

--- app.js
+++ app.js
@@ -7,18 +7,53 @@ const axios = require('axios');
 //run this app with node app.js <filename.csv> <s4Tenant:port> <user> <password>
 const inputFile = process.argv[2];
 const s4URL = process.argv[3]
+console.log(s4URL)
 
-var parser = parse({
+
+//Retrieve XSRF Token (used in POST)
+let getToken = function () {
+    return new Promise(function (resolve, reject) {  
+        console.log("Fetching CSRF token")  
+        //return x-csrf-token required to make POST requests
+        axios.request({
+            url: "/sap/opu/odata/SAP/API_SALES_ORDER_SRV/",
+            method: "HEAD",
+            baseURL: s4URL,
+            headers: {
+                "x-csrf-token": "fetch",
+                "Authorization": 'Basic '+ process.argv[4]
+              },
+        }).then((res) => {
+            console.log(res.status+" - "+res.statusText)
+            if(res.headers['x-csrf-token']){
+                console.log("CSRF-Token retrieved")
+                resolve(res.headers['x-csrf-token'])
+            }else{
+                console.error("Couldn't reetrieve CSRF Token")
+                reject(err)
+            }
+        }).catch((err) => {
+            console.error(err)
+            reject(err)
+        });
+    })
+}
+
+let parser = parse({
     delimiter: ','
 }, function (err, data) {
     async.eachSeries(data, function (line, callback) {
         var payload = csvline2SO(line)
-        console.log(payload)
+        // console.log(payload)
         callback();
     })
 });
 
-fs.createReadStream(inputFile).pipe(parser);
+getToken().then(token => {
+    fs.createReadStream(inputFile).pipe(parser);
+
+})
+
 
 function csvline2SO(line) {
     return {
@@ -43,25 +78,4 @@ function csvline2SO(line) {
             }
         ]
     }
-}
-
-
-let getToken = function () {
-    return new Promise(function (resolve, reject) {    
-        //return x-csrf-token required to make POST requests
-        axios.request({
-            url: "/sap/opu/odata/SAP/API_SALES_ORDER_SRV/",
-            method: "HEAD",
-            headers: {
-                'Authentication': 'Basic '+ process.argv[4]
-              },
-            baseURL: s4URL,
-        }).then((res) => {
-            console.log(res)
-            resolve(res.data.message)
-        }).catch((err) => {
-            console.error(err)
-            reject(err)
-        });
-    })
 }
